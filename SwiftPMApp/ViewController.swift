3269457988872ae8ba9440296e3f1f6dc245bd65
AoiSakai <aoi_sakai@icloud.com>
2021-02-13 09:17:57 +0900

    SPMAppの完成

--- SwiftPMApp/ViewController.swift
+++ SwiftPMApp/ViewController.swift
@@ -7,13 +7,20 @@
 
 import UIKit
 
+import PKHUD
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
 
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        HUD.flash(.success, delay: 2.0)
+    }
 
 }
 
